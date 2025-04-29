59b1cb285ebb0ecb693989fcf7929e5d9fb7a21b
Rafael Penacho <rafapenacho@gmail.com>
2025-04-29 17:06:49 +0100

    basic stuff

--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/model/RFP.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/model/RFP.java
@@ -1,8 +1,21 @@
 package com.example.demo.my_little_pitch.persistance.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class RFP {
 
+    @Id
+    private Long id;
+
+    
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
