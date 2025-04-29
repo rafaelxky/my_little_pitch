1d570743f8978a5b1eadc2d9eed6d8411d9f4aa7
ricardoVeloso <ricardoVeloso.cfaricardov@hotmail.com>
2025-04-29 19:46:40 +0100

    services

--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/services/RfpServiceImpl.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/services/RfpServiceImpl.java
@@ -1,7 +1,29 @@
 package com.example.demo.my_little_pitch.services;
 
-import com.example.demo.my_little_pitch.persistance.dao.jpa.UserJpaDao;
+import com.example.demo.my_little_pitch.persistance.dao.jpa.RfpJpaDao;
+import com.example.demo.my_little_pitch.persistance.model.Rfp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class RfpServiceImpl implements RfpService{
-    private UserJpaDao userDao;
+    private RfpJpaDao rfpJpaDao;
+
+    @Autowired
+    public void setRfpJpaDao(RfpJpaDao rfpJpaDao) {
+        this.rfpJpaDao = rfpJpaDao;
+    }
+
+    @Override
+    public Rfp get(Integer id) {
+        return rfpJpaDao.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public void saveOrUpdate(Rfp rfp){
+        rfpJpaDao.saveOrUpdate(rfp);
+    }
+
 }
