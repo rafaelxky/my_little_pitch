f6264d626130c585b49964ff9d9336da4d119a13
Rafael Penacho <rafapenacho@gmail.com>
2025-04-29 17:57:16 +0100

    uwhadi

--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/model/RFP.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/model/RFP.java
@@ -32,5 +32,19 @@ public class RFP {
         this.date = date;
     }
 
-    
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getTokens() {
+        return tokens;
+    }
+
+    public void setTokens(String tokens) {
+        this.tokens = tokens;
+    }
 }
