df4955c01c4bc3a83e80d9ffd60216642e6a6942
Rafael Penacho <rafapenacho@gmail.com>
2025-04-29 19:09:03 +0100

    services working

--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/controller/ExampleController.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/controller/ExampleController.java
@@ -1,5 +1,9 @@
 package com.example.demo.my_little_pitch.controller;
 
+import com.example.demo.my_little_pitch.persistance.dao.jpa.UserJpaDao;
+import com.example.demo.my_little_pitch.persistance.model.User;
+import com.example.demo.my_little_pitch.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class ExampleController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/example")
     public ResponseEntity<String> test() {
 
         String something = "hello world";
 
         return new ResponseEntity<>(something, HttpStatus.OK);
+    }
+
+    @GetMapping("/dbtest")
+    public String dbtest(){
+        User user = new User();
+        user.setEmail("wadjdwaghj@gmaiulcom");
+        user.setName("boda");
+        userService.saveOrUpdate(user);
+        return userService.get(1).toString();
     }
 
 
--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/dao/jpa/RfpJpaDao.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/persistance/dao/jpa/RfpJpaDao.java
@@ -2,7 +2,10 @@ package com.example.demo.my_little_pitch.persistance.dao.jpa;
 
 import com.example.demo.my_little_pitch.persistance.dao.RfpDao;
 import com.example.demo.my_little_pitch.persistance.model.Rfp;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public class RfpJpaDao extends GenericDao<Rfp> implements RfpDao {
 
     /**
@@ -10,7 +13,7 @@ public class RfpJpaDao extends GenericDao<Rfp> implements RfpDao {
      *
      * @param modelType the model type
      */
-    public RfpJpaDao(Class<Rfp> modelType) {
+    public RfpJpaDao() {
         super(Rfp.class);
     }
 }
