b070a3045277c8d062c67d8a2cfe4fa2f74caea1
Rafael Penacho <rafapenacho@gmail.com>
2025-04-29 20:03:32 +0100

    its 20:03 its time to go to sleep

--- my_little_pitch/src/main/java/com/example/demo/my_little_pitch/services/ResponseServiceImpl.java
+++ my_little_pitch/src/main/java/com/example/demo/my_little_pitch/services/ResponseServiceImpl.java
@@ -3,8 +3,10 @@ package com.example.demo.my_little_pitch.services;
 import com.example.demo.my_little_pitch.persistance.dao.jpa.ResponseJpaDao;
 import com.example.demo.my_little_pitch.persistance.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
 public class ResponseServiceImpl implements ResponseService{
     private ResponseJpaDao responseJpaDao;
 
